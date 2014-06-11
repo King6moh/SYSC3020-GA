edfcaf0597548bf199af62d6cca99708cad3367f
Kais93 <khassanali93@gmail.com>
2014-06-10 23:22:36 -0400

    Added comments

--- src/Application.java
+++ src/Application.java
@@ -6,8 +6,9 @@ import java.io.IOException;
 import java.util.*;
 
 /**
- * @author Kais
- *
+ * The following class will store the application within our database
+ * @author Team 4
+ * @version June 10 2014
  */
 public class Application {
 	private ArrayList<Object> application;
@@ -18,21 +19,18 @@ public class Application {
 		try {
 			applicationID = readFromFile();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		try {
 			writeToFile(applicationID);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		/* Create our application */
 		application.add(applicationID);
 		application.add(ApplicationState.OPEN.toString());
 		application.add(new Date().toString());
@@ -40,43 +38,56 @@ public class Application {
 		application.add(term.toString());
 		application.add(fieldOfStudy.toString());
 		application.add(fundingRequired);
-		try {
+		try { // add it to our database
 			writeApplicationToFile(application);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	/**
+	 * 
+	 * @return get the application ID we must assign to our application
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
 	private int readFromFile() throws FileNotFoundException, IOException {
 		BufferedInputStream in = new BufferedInputStream(new FileInputStream(System.getProperty("user.dir") + "\\Database\\LastID.txt"));
-		int availablebytes = in.available();
+		int availablebytes = in.available(); // check how big the value is
 		int offset = 1;
 		int result = 0;
 		for(int i=0; i < availablebytes-1; i++) {
 			offset = offset*10;
 		}
 		for(int i=0; i < availablebytes; i++) {
-			result = result + ((in.read()-48) * offset);
+			result = result + ((in.read()-48) * offset); // get the digits of the ID
 			offset = offset/10;
 		}
 		in.close();
 		return result;
 	}
+	/**
+	 * The application ID must be updated so we don't have any duplicates
+	 * @param applicationID application id we just assigned
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
 	private void writeToFile(int applicationID) throws FileNotFoundException, IOException {
 		FileOutputStream out = new FileOutputStream(System.getProperty("user.dir") + "\\Database\\LastID.txt");
+		// TODO make it write back the ID in a clean format
 		out.write(applicationID+49);
 		out.close();
 	}
-	
+	/**
+	 * The following method will write our application to the file
+	 * @param application the application we are to write to the database
+	 * @throws FileNotFoundException if the file we are to write to cannot be found
+	 * @throws IOException trouble writing to file
+	 */
 	private void writeApplicationToFile(ArrayList<Object> application) throws FileNotFoundException, IOException {
 		FileOutputStream out = new FileOutputStream(System.getProperty("user.dir") + "\\Database\\Applications.txt", true);
 		String data = new String( application.get(0) + "$" +  application.get(1) + "$" + application.get(2) + "$" + application.get(3) + "$" + application.get(4) + "$" + application.get(5) + "$" + application.get(6)+ "$\n");
 		out.write(data.getBytes());
-	}
-	public static void main(String [] args) {
-		Application app = new Application("KaisHassanali", Term.FALL, FieldOfStudy.ARTS, false);
 	}
 }
--- src/ApplicationDB.java
+++ src/ApplicationDB.java
@@ -8,11 +8,18 @@ import java.io.IOException;
 import java.util.*;
 
 /**
+ * The following class will act as the database controller, to access the applications and professor information (only applications implemented currently)
  * @author Team 4
  *
  */
 public class ApplicationDB {	
-	
+	/**
+	 * 
+	 * @param ID the ID of the application we want
+	 * @return the application
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
 	public static ArrayList<Object> getApplicationbyID(int ID) throws FileNotFoundException, IOException {
 		if (ID >= readFromFile())
 			return null;
@@ -20,7 +27,34 @@ public class ApplicationDB {
 			return readFromApplicationFile(ID);
 		}
 	}
-
+	
+	/**
+	 * 
+	 * @return get the application ID we must assign to our application
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	private static int readFromFile() throws FileNotFoundException, IOException {
+		BufferedInputStream in = new BufferedInputStream(new FileInputStream(System.getProperty("user.dir") + "\\Database\\LastID.txt"));
+		int availablebytes = in.available(); // check how big the value is
+		int offset = 1;
+		int result = 0;
+		for(int i=0; i < availablebytes-1; i++) {
+			offset = offset*10;
+		}
+		for(int i=0; i < availablebytes; i++) {
+			result = result + ((in.read()-48) * offset); // get the digits of the ID
+			offset = offset/10;
+		}
+		in.close();
+		return result;
+	}
+	/**
+	 * Used to get all applications corresponding to the field of study entered
+	 * @param fos the fieldofstudy we are looking for
+	 * @return
+	 * @throws IOException
+	 */
 	public static ArrayList<ArrayList> getApplicationbyFieldOfStudy(FieldOfStudy fos) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader((System.getProperty("user.dir") + "\\Database\\Applications.txt")));
 		String line;
@@ -35,7 +69,7 @@ public class ApplicationDB {
 				   if (line.charAt(i) == '$') {
 					   dollarSignCount++;
 				   }
-				   if (dollarSignCount == 5) {
+				   if (dollarSignCount == 5) { // we know the field we want is after the 5th dollar sign delimeter
 					   int index = i+1;
 					   for(int k = index; k < line.length(); k++) {
 						   if (line.charAt(k) == '$') {
@@ -47,14 +81,14 @@ public class ApplicationDB {
 					   }
 					}
 				   if (fos.toString().equals(fosLocal) == false && dollarSignCount == 5) {
-					   System.out.println(dollarSignCount);
-					   System.out.println(fos.toString());
-					   System.out.println(fosLocal);
-					   System.out.println("break line 56");
+					   //System.out.println(dollarSignCount);
+					   //System.out.println(fos.toString());
+					   //System.out.println(fosLocal);
+					  // System.out.println("break line 56");
 					   break;
 				   }
 				   if(fos.toString().equals(fosLocal) && dollarSignCount == 5) {
-					   System.out.println("line 63");
+					   //System.out.println("line 63");
 					   for(int j = 0; j < line.length(); j++) {
 						   ID = 0;
 						   if (line.charAt(j) == '$') {
@@ -68,18 +102,23 @@ public class ApplicationDB {
 							   }
 							   System.out.println(ID);
 							   arr.add(getApplicationbyID(ID));
-							   System.out.println("line 77 break");
+							  // System.out.println("line 77 break");
 							   break;
 						   }
 					   }
-					   System.out.println("Line 81 break");
+					   //System.out.println("Line 81 break");
 					   break;
 				   }
 			   }
 		}
 		return arr;
 	}
-	
+	/**
+	 * THe following method will return an array of array lists that will contain all the application that match the term given
+	 * @param term the term we are searching for
+	 * @return a list of all applications which match the term
+	 * @throws IOException
+	 */
 	public static ArrayList<ArrayList> getApplicationbyTerm(Term term) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader((System.getProperty("user.dir") + "\\Database\\Applications.txt")));
 		String line;
@@ -94,7 +133,7 @@ public class ApplicationDB {
 				   if (line.charAt(i) == '$') {
 					   dollarSignCount++;
 				   }
-				   if (dollarSignCount == 4) {
+				   if (dollarSignCount == 4) { // we know the field we want is after the 4th dollar sign delimeter
 					   int index = i+1;
 					   for(int k = index; k < line.length(); k++) {
 						   if (line.charAt(k) == '$') {
@@ -106,14 +145,14 @@ public class ApplicationDB {
 					   }
 					}
 				   if (term.toString().equals(termLocal) == false && dollarSignCount == 4) {
-					   System.out.println(dollarSignCount);
-					   System.out.println(term.toString());
-					   System.out.println(termLocal);
-					   System.out.println("break line 56");
+					   //System.out.println(dollarSignCount);
+					   //System.out.println(term.toString());
+					   //System.out.println(termLocal);
+					   //System.out.println("break line 56");
 					   break;
 				   }
 				   if(term.toString().equals(termLocal) && dollarSignCount == 4) {
-					   System.out.println("line 63");
+					  // System.out.println("line 63");
 					   for(int j = 0; j < line.length(); j++) {
 						   ID = 0;
 						   if (line.charAt(j) == '$') {
@@ -127,11 +166,11 @@ public class ApplicationDB {
 							   }
 							   System.out.println(ID);
 							   arr.add(getApplicationbyID(ID));
-							   System.out.println("line 77 break");
+							   //System.out.println("line 77 break");
 							   break;
 						   }
 					   }
-					   System.out.println("Line 81 break");
+					  // System.out.println("Line 81 break");
 					   break;
 				   }
 			   }
@@ -139,28 +178,18 @@ public class ApplicationDB {
 		return arr;
 		
 	}
-	
-	private static int readFromFile() throws FileNotFoundException, IOException {
-		BufferedInputStream in = new BufferedInputStream(new FileInputStream(System.getProperty("user.dir") + "\\Database\\LastID.txt"));
-		int availablebytes = in.available();
-		int offset = 1;
-		int result = 0;
-		for(int i=0; i < availablebytes-1; i++) {
-			offset = offset*10;
-		}
-		for(int i=0; i < availablebytes; i++) {
-			result = result + ((in.read()-48) * offset);
-			offset = offset/10;
-		}
-		in.close();
-		return result;
-	}
+	/**
+	 * The following method is used to retreive the application with the ID number that is specified
+	 * @param ID the ID of the application we are looking for
+	 * @return the application in arraylist format
+	 * @throws IOException
+	 */
 	private static ArrayList<Object> readFromApplicationFile(int ID) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader((System.getProperty("user.dir") + "\\Database\\Applications.txt")));
 		String line;
 		int digits = 0;
 		int number = ID;
-		
+		/* fields used to store the information about the application */
 		int applicationID;
 		String state = null;
 		String date = null;
@@ -184,49 +213,49 @@ public class ApplicationDB {
 		while ((line = br.readLine()) != null) {
 		   for(int i = 0; i < line.length(); i++) {
 			   if (i != digits) {
-				   if (line.charAt(i) != IDinBytes[i] && line.charAt(i) != '$') {
+				   if (line.charAt(i) != IDinBytes[i] && line.charAt(i) != '$') { // check if the id matches
 					   break;
 				   }
 			   }
 			   else if (line.charAt(i) == '$') {
 				   applicationID = ID;
 				   int index = i+1;
-				   for(int k = index; k < line.length(); k++) {
+				   for(int k = index; k < line.length(); k++) { // get the application state
 					   if (line.charAt(k) == '$') {
 						   state = line.substring(index, k);
 						   index = k+1;
 						   break;
 					   }
 				   }
-				   for(int k = index; k < line.length(); k++) {
+				   for(int k = index; k < line.length(); k++) { // get the application date
 					   if (line.charAt(k) == '$') {
 						   date = line.substring(index, k);
 						   index = k+1;
 						   break;
 					   }
 				   }
-				   for(int k = index; k < line.length(); k++) {
+				   for(int k = index; k < line.length(); k++) { // get the name of the applicant
 					   if (line.charAt(k) == '$') {
 						   name = line.substring(index, k);
 						   index = k+1;
 						   break;
 					   }
 				   }
-				   for(int k = index; k < line.length(); k++) {
+				   for(int k = index; k < line.length(); k++) { // get the term for the application
 					   if (line.charAt(k) == '$') {
 						   term = line.substring(index, k);
 						   index = k+1;
 						   break;
 					   }
 				   }
-				   for(int k = index; k < line.length(); k++) {
+				   for(int k = index; k < line.length(); k++) { // get the applicants field of study
 					   if (line.charAt(k) == '$') {
 						   fieldOfStudy = line.substring(index, k);
 						   index = k+1;
 						   break;
 					   }
 				   }
-				   for(int k = index; k < line.length(); k++) {
+				   for(int k = index; k < line.length(); k++) { // get whether or not the applicant needs funding
 					   if (line.charAt(k) == '$') {
 						   fundingRequired = line.substring(index, k);
 						   index = k+1;
@@ -242,7 +271,7 @@ public class ApplicationDB {
 				   application.add(fieldOfStudy);
 				   application.add(fundingRequired);
 				   br.close();
-				   return application;
+				   return application; // return the application information is arraylist format
 			   }
 			   //System.out.println("didn't find tab");
 		   }
@@ -250,15 +279,27 @@ public class ApplicationDB {
 		br.close();
 		return null;
 	}
+	/**
+	 * The following main is just to show that our methods defined above work. Everytime it is run, it will append 2 applications to the database
+	 * @param args
+	 */
 	public static void main(String [] args) {
+		Application app = new Application("KaisHassanali", Term.FALL, FieldOfStudy.ENGINEERING, false);
+		app = new Application("SamsonTruong", Term.WINTER, FieldOfStudy.SCIENCE, true);
 		ArrayList<ArrayList> array = new ArrayList<ArrayList>();
 		try {
 			array = getApplicationbyFieldOfStudy(FieldOfStudy.ENGINEERING);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.print(array);
+		try {
+			array = getApplicationbyTerm(Term.WINTER);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		System.out.print(array);
